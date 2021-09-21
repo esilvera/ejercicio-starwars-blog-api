24bd6b725f00b95748ef6d781434cfd10c5a206c
Edwards Silvera <edwards.silvera@gmail.com>
2021-09-21 21:45:30 +0000

    Trabajando Tabla Planets

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Character, Planet
 #from models import Person
 
 app = Flask(__name__)
@@ -36,8 +36,103 @@ def handle_hello():
     response_body = {
         "msg": "Hello, this is your GET /user response "
     }
-
     return jsonify(response_body), 200
+
+@app.route('/api/character', methods=['GET'])
+def get_characters():
+    characters = Character.query.all()
+    characters = list(map(lambda character: character.serialize(), characters))
+    return jsonify(characters), 200
+
+@app.route('/api/character', methods=['POST'])
+def post_characters():
+
+    name = request.json.get('name')
+    hair_color = request.json.get('hair_color')
+    eye_color = request.json.get('eye_color')
+    gender = request.json.get('gender')
+    description = request.json.get('description')
+    planet_id = request.json.get('planet_id')
+
+    character = Character()
+    character.name = name
+    character.hair_color = hair_color
+    character.eye_color = eye_color
+    character.gender = gender
+    character.description = description
+    character.planet_id = planet_id
+    character.save()
+
+    return jsonify(character.serialize()), 201
+
+@app.route('/api/character/<int:id>', methods=['PUT'])
+def put_characters(id):
+
+    name = request.json.get('name')
+    hair_color = request.json.get('hair_color')
+    eye_color = request.json.get('eye_color')
+    gender = request.json.get('gender')
+    description = request.json.get('description')
+    planet_id = request.json.get('planet_id')
+
+    character = Character.query.get(id)
+    character.name = name
+    character.hair_color = hair_color
+    character.eye_color = eye_color
+    character.gender = gender
+    character.description = description
+    character.planet_id = planet_id
+    character.update()
+
+    return jsonify(character.serialize()), 200
+
+@app.route('/api/character/<int:id>', methods=['DELETE'])
+def delete_characters(id):
+
+    character = Character.query.get(id)
+
+    if not character: return jsonify({"status": False, "msg": "Character doesn't exist"}), 404
+
+    character.delete()
+
+    return jsonify({"status": True, "msg": "Character deleted"}), 200
+
+@app.route('/api/planet', methods=['GET'])
+def get_planets():
+    planets = Planet.query.all()
+    planets = list(map(lambda planet: planet.serialize(), planets))
+    return jsonify(planets), 200
+
+@app.route('/api/planet', methods=['POST'])
+def post_planets():
+
+    name = request.json.get('name')
+    diameter = request.json.get('diameter')
+    rotation_period = request.json.get('rotation_period')
+    population = request.json.get('population')
+    climate = request.json.get('climate')
+    terrain = request.json.get('terrain')
+    #characters = request.json.get('characters') # No va porque se asigna con el character
+
+    planet = Planet()
+    planet.name = name
+    planet.diameter = diameter
+    planet.rotation_period = rotation_period
+    planet.population = population
+    planet.climate = climate
+    planet.terrain = terrain
+    #planet.characters = characters
+    planet.save()
+
+    return jsonify(planet.serialize()), 201
+
+
+
+
+
+
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
@@ -17,3 +18,81 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class Character(db.Model):
+    __tablename__ = 'characters'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    hair_color = db.Column(db.String(50), default="")
+    eye_color = db.Column(db.String(50), default="")
+    gender = db.Column(db.String(50), default="")
+    description = db.Column(db.String(250), default="")
+    planet_id = db.Column(db.Integer, db.ForeignKey("planets.id"), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "hair_color": self.hair_color,
+            "eye_color": self.eye_color,
+            "gender": self.gender,
+            "description": self.description,
+            "homeworld": self.planet.name #traemos de la tabla planeta el nombre del planeta
+            
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class Planet(db.Model):
+    __tablename__ = 'planets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    diameter = db.Column(db.String(150), default="")
+    rotation_period = db.Column(db.String(50), default="")
+    population = db.Column(db.String(150), default="")
+    climate = db.Column(db.String(150), default="")
+    terrain = db.Column(db.String(150), default="")
+    characters = db.relationship("Character", backref="planet")
+    
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "diameter": self.diameter,
+            "rotation_period": self.rotation_period,
+            "population": self.population,
+            "climate": self.climate,
+            "terrain": self.terrain
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
